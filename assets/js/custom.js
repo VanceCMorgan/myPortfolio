aef97af9761784273735723ebde25c6e75aa1560
VanceCMorgan <vance.morgan1@mohawkcollege.ca>
2019-07-27 10:50:38 -0400

    Added gallery for project images --still not fully functional

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -47,3 +47,64 @@ function learnMore($index) {
       } 
 }
 
+
+var firstSlide =document.getElementById("firstSlide");
+var secondSlide = document.getElementById("secondSlide");
+var thirdSlide = document.getElementById("thirdSlide");
+$screenShots = ["url(images/screenshots/emulation1.png)","url(images/screenshots/emulation2.png)","url(images/screenshots/emulation3.png)","url(images/screenshots/emulation4.png)","url(images/screenshots/emulation5.png)"];
+$i = 0;
+function shuffle($option) {
+    
+    if($option == 1 && $i < $screenShots.length - 3){
+        $i++;
+    }
+    if($option == -1 && $i >= 1){
+        $i--;
+    }
+
+    firstSlide.style.backgroundImage = $screenShots[$i]; 
+    secondSlide.style.backgroundImage = $screenShots[Math.max($i + 1, 1)];
+    thirdSlide.style.backgroundImage = $screenShots[Math.max($i + 2, 2)];
+}
+
+window.onload = shuffle();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//get the div that will hold large image
+var imgHolder = document.getElementById("img-holder");
+
+// When the user clicks on the button, open the modal
+function blowUp($choice) {
+  modal.style.display = "block";
+  imgHolder.style.backgroundRepeat = "no-repeat";
+  switch($choice){
+      case 1:
+          imgHolder.style.backgroundImage =firstSlide.style.backgroundImage;
+          break;
+      case 2:
+          imgHolder.style.backgroundImage = secondSlide.style.backgroundImage;
+          break;
+      case 3:
+          imgHolder.style.backgroundImage = thirdSlide.style.backgroundImage;
+      break;
+    default:
+  }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
