21161ce2f4ad268f6a1bc395491f04714ccb5672
vancemorgan <vance_morgan@hotmail.com>
2020-05-20 09:32:21 -0400

    link updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,6 @@
 function learnMore($index) {
 
     $currentSet = null;
-
     switch($index) {
         case -1:
             $('.features').fadeIn(300); 
@@ -11,35 +10,47 @@ function learnMore($index) {
             $('.project4').fadeOut(300);
             $('.project5').fadeOut(300);
             $('#goBackBtn').fadeOut(300);
+            $('#projectCaption').fadeIn(300); 
+            $('#projectTitle').fadeIn(300); 
           break;
         case 1:
             $('.features').fadeOut(300); 
             $('.project1').fadeIn(300);
             $('#goBackBtn').fadeIn(300);
+            $('#projectCaption').fadeOut(300); 
+            $('#projectTitle').fadeOut(300); 
             $currentSet =  $('.project1');
           break;
           case 2:
             $('.features').fadeOut(300); 
             $('.project2').fadeIn(300);
             $('#goBackBtn').fadeIn(300);
+            $('#projectCaption').fadeOut(300); 
+            $('#projectTitle').fadeOut(300); 
             $currentSet =  $('.project2');
           break;
         case 3:
             $('.features').fadeOut(300); 
             $('.project3').fadeIn(300);
             $('#goBackBtn').fadeIn(300);
+            $('#projectCaption').fadeOut(300); 
+            $('#projectTitle').fadeOut(300); 
             $currentSet =  $('.project3');
           break;
           case 4:
             $('.features').fadeOut(300); 
             $('.project4').fadeIn(300);
             $('#goBackBtn').fadeIn(300);
+            $('#projectCaption').fadeOut(300); 
+            $('#projectTitle').fadeOut(300); 
             $currentSet =  $('.project4');
           break;
         case 5:
             $('.features').fadeOut(300); 
             $('.project5').fadeIn(300);
             $('#goBackBtn').fadeIn(300);
+            $('#projectCaption').fadeOut(300); 
+            $('#projectTitle').fadeOut(300); 
             $currentSet =  $('.project5');
           break;
         default:
