fb5cbb78561bb9d425a28fc65f6cf8e9b11da6da
vancemorgan <vance_morgan@hotmail.com>
2020-05-20 15:03:05 -0400

    gallaraycaption fix

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -2,7 +2,7 @@ function openImage(imgs,projectNumber) {
     // Get the expanded image
     var expandImg = document.getElementsByClassName("expandedImg")[projectNumber-1];
     // Get the image text
-    var imgText = document.getElementById("imgtext");
+    var imgText = document.getElementsByClassName("imgtext")[projectNumber-1];
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
     // Use the value of the alt attribute of the clickable image as text inside the expanded image
